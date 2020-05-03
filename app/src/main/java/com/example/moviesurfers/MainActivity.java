289a797ab037408153480555df0763fb31a8b8e9
Nw1817 <niknik001@live.com>
2020-05-03 14:09:23 -0500

    Added button to go to Favorites Activity

--- app/src/main/java/com/example/moviesurfers/MainActivity.java
+++ app/src/main/java/com/example/moviesurfers/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.moviesurfers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, ResultActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent(this, FavoritesActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
